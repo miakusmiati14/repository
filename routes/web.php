9f7a2f3910cafd045f8180ad7514e93aea333595
miakusmiati14 <kmiaaa14@gmail.com>
2023-11-26 20:12:55 +0700

    subimison 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use illuminate\Support\Facades\auth;
+Use illuminate\support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +18,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth::routes();
+
+// Route::get('/home', HomeController@index');
+
+route::get('/user', [UserController::class, 'index'])->name('user');
+
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::get('/user/create/read', [UserController::class, 'read'])->name('user.create.read');
+Route::get('/user/create/read/update', [UserController::class, 'update'])->name('user.create.read.update');
+Route::get('/user/create/read/update/delete', [UserController::class, 'delete'])->name('user.create.read.update/delete');
